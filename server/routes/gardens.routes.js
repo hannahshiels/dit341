ff0e578e4d0a52172d25f69bb009221d684b8f74
Himank Meattle <himank@student.chalmers.se>
2021-09-29 21:08:05 +0200

    adding missing routes in garden

--- server/routes/gardens.routes.js
+++ server/routes/gardens.routes.js
@@ -13,4 +13,14 @@ router.route('/api/users/:userID/gardens/:gardenID')
     .patch(gardenControllers.partialUpdateGarden)
     .delete(gardenControllers.deleteGarden)
 
+router.route('/api/gardens')
+    .get(gardenControllers.getAllGardens)
+    .post(gardenControllers.createGarden)
+
+router.route('/api/gardens/:gardenID')
+    .get(userControllers.getGarden)
+    .put(userControllers.fullyUpdateGarden)
+    .patch(userControllers.partialUpdateGarden)
+    .delete(userControllers.deleteGarden)
+
 module.exports = router;
