febeeef8f50feab665a19416076416e1a9ff74d9
puerto <puerto@student.chalmers.se>
2021-09-30 10:41:22 +0200

    Added ad router

--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Ads from './views/Ads.vue'
 
 Vue.use(Router)
 
@@ -12,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/ads',
+      name: 'ads',
+      component: Ads
     }
   ]
 })
