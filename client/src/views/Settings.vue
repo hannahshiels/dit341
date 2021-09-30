1eddafd3661f0fa50d9d1a8942c2d488c766a26b
hannahshiels <shiels@student.chalmers.se>
2021-09-30 20:47:51 +0200

    update, delete account

--- client/src/views/Settings.vue
+++ client/src/views/Settings.vue
@@ -1,38 +1,51 @@
 <template>
   <div>
-    <b-container>
+    <b-container class="text-black-50 p-2">
       <b-row align-h="center">
-        <p>{{ user_data.name }}</p>
+        <b-col xl="6" md="8" sm="12">
+          <update-acc-form />
+        </b-col>
       </b-row>
-      <b-row align-h="center">
-        <p>{{ user_data.email }}</p>
-      </b-row>
-      <b-row align-h="center">
-        <p>{{ user_data.password }}</p>
+      <b-row class="mt-5" align-h="center">
+        <b-col class="text-center"  xl="6" md="8" sm="12">
+          <b-button @click="onClick" class="btn-lg btn-danger"> Delete Account </b-button>
+        </b-col>
       </b-row>
     </b-container>
   </div>
 </template>
 
+<style scoped></style>
+
 <script>
 import { Api } from '@/Api'
+import Router from '@/router'
+import UpdateAccForm from '../components/UpdateAccForm'
 
 export default {
-  mounted() {
-    Api.get('/users/' + this.user_id)
-      .then(response => {
-        console.log(response)
-        this.user_data = response.data
-      })
-      .catch(error => {
-        console.log(error)
-      })
+  components: {
+    UpdateAccForm
   },
-
   data() {
     return {
-      user_id: this.$parent.user_id,
-      user_data: {}
+      user_id: this.$parent.user_id
+    }
+  },
+  methods: {
+    onClick() {
+      this.deleteAccount()
+      this.$parent.setAuthenticated(false)
+      this.$parent.setID('')
+      Router.push('/')
+    },
+    deleteAccount() {
+      Api.delete('users/' + this.user_id)
+        .then(response => {
+          console.log(response)
+        })
+        .catch(error => {
+          console.log(error)
+        })
     }
   }
 }
