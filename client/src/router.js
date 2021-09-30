6d2d3c36a370c400dfd5a81d510580e6d097b3ac
puerto <puerto@student.chalmers.se>
2021-09-30 15:53:34 +0200

    Added start route in router file

--- client/src/router.js
+++ client/src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Ads from './views/Ads.vue'
+import Start from './views/Start.vue'
 
 Vue.use(Router)
 
@@ -18,6 +19,11 @@ export default new Router({
       path: '/ads',
       name: 'ads',
       component: Ads
+    },
+    {
+      path: '/start',
+      name: 'start',
+      component: Start
     }
   ]
 })
