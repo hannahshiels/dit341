82fbf0dde03c5e5949450b3f682570e02148bca8
puerto <puerto@student.chalmers.se>
2021-10-07 17:43:48 +0200

    Small change

--- client/src/views/SingleAd.vue
+++ client/src/views/SingleAd.vue
@@ -6,6 +6,7 @@
         <h3 class="d-flex justify-content-center">Description: {{ this.description }}</h3>
         <h3 class="d-flex justify-content-center">Date posted: {{ this.datePosted }}</h3>
         <h3 class="d-flex justify-content-center">Uploaded by: {{ this.uploadedBy }}</h3>
+        <h3 class="d-flex justify-content-center">User name: {{ this.userName}}</h3>
         <h3 class="d-flex justify-content-center">Contact number: {{ this.contactNumber }}</h3>
         <h3 class="d-flex justify-content-center">Contact address: {{ this.contactAddress }}</h3>
       </div>
@@ -18,7 +19,7 @@ export default {
   data() {
     return {
       id: this.$route.params.id,
-      // userName: '',
+      userName: this.$parent.user_id,
       type: 'Generic',
       description: 'Generic',
       contactNumber: 'Generic',
@@ -28,8 +29,6 @@ export default {
     }
   },
   mounted() {
-    // Add when user is logged in
-    /*
     Api.get('/users/6159e42b86f6ad3ed2cf3811')
       .then(response => {
         this.userName = response.data.name
@@ -37,7 +36,6 @@ export default {
       .catch(error => {
         console.log(error)
       })
-    */
 
     Api.get('/users/6159e42b86f6ad3ed2cf3811/ads/' + this.id)
       .then(response => {
@@ -47,7 +45,7 @@ export default {
         this.datePosted = response.data.ad_date_posted
         this.uploadedBy = response.data.uploaded_by
         this.contactNumber = response.data.ad_contact[0].number
-        this.contactAddress = response.data.ad_contact[1].address
+        this.contactAddress = response.data.ad_contact[0].address
       })
       .catch(error => {
         console.log(error)
