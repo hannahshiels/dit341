330cab56961ff2c6e7793318f6a04b7130e2596d
puerto <puerto@student.chalmers.se>
2021-10-12 14:30:36 +0200

    Fixed logged out start view

--- client/src/components/GardenSlide.vue
+++ client/src/components/GardenSlide.vue
@@ -4,7 +4,7 @@
     <div class="carousel-item active">
       <img :src= "this.first_img" class="d-block w-100" alt="">
     </div>
-    <div v-for="garden in gardens.slice(1, this.gardens.length)" v-bind:key="garden._id" class="carousel-item">
+    <div v-for="garden in gardens" v-bind:key="garden._id" class="carousel-item">
       <img :src="garden.img_link" class="d-block w-100" alt="">
     </div>
   </div>
@@ -24,26 +24,30 @@
 import { Api } from '@/Api'
 
 export default {
-  name: 'gardens',
   data() {
     return {
       user_id: this.$parent.user_id,
-      first_img: ''
+      first_img: '',
+      gardens: []
     }
   },
   mounted() {
-    Api.get('/users/' + this.user_id + '/gardens')
-      .then(response => {
-        console.log(response)
-        console.log('Current user ID is ' + this.user_id)
-        this.gardens = response.data.User_gardens
-        console.log(this.gardens[0].img_link)
-        this.first_img = this.gardens[0].img_link
-      })
-      .catch(error => {
-        this.ads = []
-        console.log(error)
-      })
+    if (this.user_id === '') {
+      this.first_img = 'https://static.thenounproject.com/png/504708-200.png'
+    } else {
+      Api.get('/users/' + this.user_id + '/gardens')
+        .then(response => {
+          console.log(response)
+          console.log('Current user ID is ' + this.user_id)
+          this.gardens = response.data.User_gardens
+          console.log(this.gardens[0].img_link)
+          this.first_img = this.gardens[0].img_link
+        })
+        .catch(error => {
+          this.gardens = []
+          console.log(error)
+        })
+    }
   }
 }
 </script>
